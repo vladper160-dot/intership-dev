38c89a0c29ebd765801d9ace4f3a64b1ffd22d44
CreSilver <davidbocek2005@gmail.com>
2026-05-22 13:37:11 +0200

    add secbar

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,6 +2,7 @@ import { useFetchData } from '../../hooks/useFetchData';
 import { DataTable } from '../DataTable/DataTable';
 import { Sidebar } from '../Sidebar/Sidebar';
 import Navbar from '../Navbar/Navbar';
+import { Secbar } from '../Secbar/secbar';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="app-container">
       <Navbar />
+      <Secbar />
       <Sidebar />
       <div className="main-container">
         <DataTable data={data} />
---
+++ frontend/src/components/Secbar/secbar.tsx
@@ -0,0 +1,11 @@
+export function Secbar() {
+
+
+
+
+    return (
+        <section>
+            <button></button>
+        </section>
+    )
+}
