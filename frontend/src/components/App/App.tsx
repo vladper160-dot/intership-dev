0d8a560a2157663dbf357cebc943f0c99bb48578
Vladislav Perďoch <vladper160@gmail.com>
2026-05-22 13:30:42 +0200

    navbar

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useFetchData } from '../../hooks/useFetchData';
 import { DataTable } from '../DataTable/DataTable';
 import { Sidebar } from '../Sidebar/Sidebar';
-import Navbar from '../components/Navbar/Navbar';
+import Navbar from '../Navbar/Navbar';
 import './App.css';
 
 function App() {
